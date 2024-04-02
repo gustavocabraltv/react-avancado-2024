7dcdbb81633d66dac811b1dc04af669684daf8b4
Gustavo Cabral <studiogustavocabral@gmail.com>
2024-04-02 16:03:21 -0300

    test

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -8,14 +8,25 @@
 
 
 
-const path = require('path');
+// const path = require('path');
 
-const buildEslintAndPrettierCommand = (filenames) => {
-  const prettierCommand = `prettier --write ${filenames.join(' ')}`;
-  const eslintCommand = `npm run lint --fix . ${filenames.map((f) => `--file ${path.relative(process.cwd(), f)}`).join(' ')}`;
-  return [prettierCommand, eslintCommand];
-};
+// const buildEslintAndPrettierCommand = (filenames) => {
+//   const prettierCommand = `prettier --write ${filenames.join(' ')}`;
+//   const eslintCommand = `npm run lint --fix . ${filenames.map((f) => `--file ${path.relative(process.cwd(), f)}`).join(' ')}`;
+//   return [prettierCommand, eslintCommand];
+// };
 
+// module.exports = {
+//   '*.{js,jsx,ts,tsx}': buildEslintAndPrettierCommand,
+// };
+
+const path = require('path')
+ 
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`
+ 
 module.exports = {
-  '*.{js,jsx,ts,tsx}': buildEslintAndPrettierCommand,
-};
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+}
