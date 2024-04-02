a20a789b92d476a39bcc9efa2c2f0686c508f9b9
Gustavo Cabral <studiogustavocabral@gmail.com>
2024-04-02 16:01:02 -0300

    test

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,21 @@
+// module.exports = {
+//   // Lint & Prettify TS and JS files
+//   '*.{js,jsx,ts,tsx}': (filenames) => [
+//     `prettier --write ${filenames.join(' ')}`,
+//     `npm run lint --fix . ${filenames.join(' --file')}`
+//   ]
+// }
+
+
+
+const path = require('path');
+
+const buildEslintAndPrettierCommand = (filenames) => {
+  const prettierCommand = `prettier --write ${filenames.join(' ')}`;
+  const eslintCommand = `npm run lint --fix . ${filenames.map((f) => `--file ${path.relative(process.cwd(), f)}`).join(' ')}`;
+  return [prettierCommand, eslintCommand];
+};
+
 module.exports = {
-  // Lint & Prettify TS and JS files
-  '*.{js,jsx,ts,tsx}': (filenames) => [
-    `prettier --write ${filenames.join(' ')}`,
-    `npm run lint --fix . ${filenames.join(' --file')}`
-  ]
-}
+  '*.{js,jsx,ts,tsx}': buildEslintAndPrettierCommand,
+};
