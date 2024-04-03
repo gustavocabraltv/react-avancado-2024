9a4b7f661be759f50c3d1a5d5f019cd0babee825
Gustavo Cabral <studiogustavocabral@gmail.com>
2024-04-02 23:25:14 -0300

    jest tests

--- src/Components/Main/test.tsx
+++ src/Components/Main/test.tsx
@@ -1,9 +1,20 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import Main from '.'
 
 describe('<Main />', () => {
   it('should render the heading', () => {
-    render(<Main />)
+    // renderiza o component
+
+    // render(<Main />)
+    const { container } = render(<Main />)
+
+    // busca o elemento e verifica a existência dele
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument()
+
+    // gerar snapshot
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
