887fc91c36eb46bd4e38d63e8272b844a45f5677
Gustavo Cabral <studiogustavocabral@gmail.com>
2024-04-13 12:13:46 -0300

    Media Match

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -2,11 +2,19 @@ import * as S from './styles'
 
 export type HeadingProps = {
   children: React.ReactNode
+  color?: 'white' | 'black'
+  lineLeft?: boolean
+  lineBottom?: boolean
 }
 
-const Heading = ({ children }: HeadingProps) => (
-  <S.Wrapper>
-    <h1>{children}</h1>
+const Heading = ({
+  children,
+  color = 'white',
+  lineLeft = false,
+  lineBottom = false
+}: HeadingProps) => (
+  <S.Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
+    {children}
   </S.Wrapper>
 )
 
--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -1,6 +1,32 @@
-import styled, { css } from 'styled-components'
-export const Wrapper = styled.h2`
-  ${({ theme }) => css`
+import styled, { DefaultTheme, css } from 'styled-components'
+import { HeadingProps } from '.'
+
+const wrapperModifiers = {
+  lineLeft: (theme: DefaultTheme) => css`
+    padding-left: ${theme.spacings.xxsmall};
+    border-left: 0.7rem solid ${theme.colors.secondary};
+  `,
+  lineBottom: (theme: DefaultTheme) => css`
+    position: relative;
+    margin-bottom: ${theme.spacings.medium};
+
+    &::after {
+      position: absolute;
+      left: 0;
+      bottom: -1rem;
+      content: '';
+      width: 5rem;
+      border-bottom: 0.7rem solid ${theme.colors.primary};
+    }
+  `
+}
+
+export const Wrapper = styled.h2<HeadingProps>`
+  ${({ theme, color, lineLeft, lineBottom }) => css`
+    color: ${theme.colors[color!]};
     font-size: ${theme.font.sizes.medium};
+
+    ${lineLeft && wrapperModifiers.lineLeft(theme)}
+    ${lineBottom && wrapperModifiers.lineBottom(theme)}
   `}
 `
